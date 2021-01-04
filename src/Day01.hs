b23679518955964219893ffdd7f4c7baaac0bb75
Roland Tritsch <roland@tritsch.org>
2021-01-04 08:29:10 +0000

    DAy01: Make linter happy.

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,33 +1,31 @@
-{- |
-Problem: <https://adventofcode.com/2020/day/1>
-
-Solution:
-
-General - List combination/search problem. Build all combination
-of list elements. And lock for the ones that sum up to be 2020.
-
-Part 1 - Do it with pairs.
-
-Part 2 - Do it with triplets. Note: To make the search space small,
-we first search the expenses in reverse order and then stop searching,
-when the first 2 numbers are already bigger than 2020.
--}
+-- |
+-- Problem: <https://adventofcode.com/2020/day/1>
+--
+-- Solution:
+--
+-- General - List combination/search problem. Build all combination
+-- of list elements. And lock for the ones that sum up to be 2020.
+--
+-- Part 1 - Do it with pairs.
+--
+-- Part 2 - Do it with triplets. Note: To make the search space small,
+-- we first search the expenses in reverse order and then stop searching,
+-- when the first 2 numbers are already bigger than 2020.
 module Day01 where
 
-import Text.Megaparsec (manyTill, eof, optional)
+import Text.Megaparsec (eof, manyTill, optional)
 import Text.Megaparsec.Char (newline)
-
-import Util (inputRaw, inputParser, Parser, integer)
+import Util (Parser, inputParser, inputRaw, integer)
 
 type Expense = Int
 
 -- | read the input file.
 input :: String -> [Expense]
-input filename = (map read . lines . inputRaw) filename
+input = map read . lines . inputRaw
 
 -- | the parsed input.
 parsedInput :: String -> [Expense]
-parsedInput filename = inputParser parseExpenses filename
+parsedInput = inputParser parseExpenses
 
 -- | parse the expenses.
 parseExpenses :: Parser [Expense]
@@ -37,9 +35,11 @@ parseExpense :: Parser Int
 parseExpense = integer
 
 part1 :: [Expense] -> Int
-part1 expenses = x * y where
-    (x, y) = head $ [(x',y') | x' <- expenses, y' <- expenses, x' + y' == 2020]
-    
+part1 expenses = x * y
+  where
+    (x, y) = head $ [(x', y') | x' <- expenses, y' <- expenses, x' + y' == 2020]
+
 part2 :: [Expense] -> Int
-part2 expenses = x * y * z where
-    (x, y, z) = head $ [(x',y',z') | x' <- expenses, y' <- expenses, x' + y' <= 2020, z' <- expenses, x' + y' + z' == 2020]
+part2 expenses = x * y * z
+  where
+    (x, y, z) = head $ [(x', y', z') | x' <- expenses, y' <- expenses, x' + y' <= 2020, z' <- expenses, x' + y' + z' == 2020]
