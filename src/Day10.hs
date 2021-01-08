fed03b30ff9083765e89b51b4860b43c11776114
Roland Tritsch <roland@tritsch.org>
2021-01-08 15:52:54 +0000

    Day10: Solved.

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,36 +1,33 @@
-{-|
-Problem: <https://adventofcode.com/2020/day/10>
-
-Solution:
-
-General - Connect jolts/volts. The trick here is to read the problem
-description carefully. You then realize that you can solve the problem
-by building the diffs between 2 consecutive numbers and just count the
-number of 1 and 3 diffs.
-
-Part 1 - Multiply the number of ones and threes.
-
-Part 2 - ???
--}
+-- |
+-- Problem: <https://adventofcode.com/2020/day/10>
+--
+-- Solution:
+--
+-- General - Connect jolts/volts. The trick here is to read the problem
+-- description carefully. You then realize that you can solve the problem
+-- by building the diffs between 2 consecutive numbers and just count the
+-- number of 1 and 3 diffs.
+--
+-- Part 1 - Multiply the number of ones and threes.
+--
+-- Part 2 - ???
 module Day10 where
 
+import Data.List (sort)
+import Util (inputRaw)
 import Prelude
 
-import Data.List (init, maximum)
-import Data.String (lines)
-import Data.Binary (toStr)
-
-type Jolt = Integer
+type Jolt = Int
 
 input :: String -> [Jolt]
-input filename = map read contents where
-    contents = init $ lines $ toStr $ unsafePerformIO $ readFile filename
+input filename = map read $ lines $ inputRaw filename
 
-part1 :: [Jolt] -> Integer
-part1 jolts = count 1 * count 3 where
-    count n = length $ filter (\d -> d == n) diffs where
-        jolts' = sort $ jolts ++ [0, (maximum jolts) + 3]
-        diffs = map (\(a, b) -> b - a) $ zip (init jolts') (tail jolts')
+part1 :: [Jolt] -> Int
+part1 jolts = count 1 * count 3
+  where
+    count n = length $ filter (== n) diffs
+    diffs = map (\(a, b) -> b - a) $ zip (init jolts') (tail jolts')
+    jolts' = sort $ jolts ++ [0, (maximum jolts) + 3]
 
-part2 :: [Jolt] -> Integer
+part2 :: [Jolt] -> Int
 part2 jolts = length jolts
