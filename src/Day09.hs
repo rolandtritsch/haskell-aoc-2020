13fff6884933a89ca5b886cd9c6b9789cf10fadb
Roland Tritsch <roland@tritsch.org>
2021-01-08 13:31:35 +0000

    Day09: Solved.

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,48 +1,41 @@
-{-|
-Problem: <https://adventofcode.com/2020/day/9>
-
-Solution:
-
-General - Get a/the list of numbers. Set the length of the preamble (P).
-Push a window of length P over the list and check the next number (by
-calcing the sum of pairs until we find a match).
-
-Part 1 - ???
-
-Part 2 - ???
--}
+-- |
+-- Problem: <https://adventofcode.com/2020/day/9>
+--
+-- Solution:
+--
+-- General - Get a/the list of numbers. Set the length of the preamble (P).
+-- Push a window of length P over the list and check the next number (by
+-- calcing the sum of pairs until we find a match).
+--
+-- Part 1 - Calc the pairs recursively.
+--
+-- Part 2 - ???
 module Day09 where
 
+import Util (inputRaw)
 import Prelude
 
-import Data.List (init, (!!))
-import Data.String (lines)
-import Data.Binary (toStr)
-
-type XMAS = {
-    preamble :: Integer,
-    numbers :: [Integer]
-}
+data XMAS = XMAS Int [Int]
 
 input :: String -> XMAS
-input filename = {preamble = head contents, numbers = tail contents} where
-    contents = map read $ init $ lines $ toStr $ unsafePerformIO $ readFile filename
+input filename = XMAS preamble numbers
+  where
+    (preamble : numbers) = map read $ lines $ inputRaw filename
 
-isNotEmpty :: forall a. [a] -> Bool
-isNotEmpty [] = false
-isNotEmpty _ = true
-
-check :: [Integer] -> Integer -> Bool
-check xmas preamble = isNotEmpty pairs where
+check :: [Int] -> Int -> Bool
+check xmas preamble = not (null pairs)
+  where
     n = xmas !! preamble
     ns = take preamble xmas
-    pairs = [(x,y) | x <- ns, y <- ns, x > y, x + y == n] 
+    pairs = [(x, y) | x <- ns, y <- ns, x > y, x + y == n]
 
-part1 :: XMAS -> Integer
-part1 xmas = go xmas.numbers xmas.preamble (check xmas.numbers xmas.preamble) where
-  go ns p false = ns !! p
-  go ns p true = go ns' p (check ns' p) where
-      ns' = tail ns
+part1 :: XMAS -> Int
+part1 (XMAS preamble numbers) = go numbers preamble (check numbers preamble)
+  where
+    go ns p False = ns !! p
+    go ns p True = go ns' p (check ns' p)
+      where
+        ns' = tail ns
 
-part2 :: XMAS -> Integer
-part2 xmas = length xmas.numbers
+part2 :: XMAS -> Int
+part2 (XMAS _ numbers) = length numbers
