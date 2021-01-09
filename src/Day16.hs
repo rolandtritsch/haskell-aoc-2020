41fdc37bf780792e7353125df6c781087f51f547
Roland Tritsch <roland@tritsch.org>
2021-01-09 11:42:30 +0000

    Day16: Solved.

--- src/Day16.hs
+++ src/Day16.hs
@@ -15,39 +15,36 @@ Part 2 - ???
 module Day16 where
 
 import Prelude
-
-import Data.List (init, sum, nub, notElem, (!!))
-import Data.String (lines, split)
-import Data.Binary (toStr)
+import Util (inputRaw)
+import Data.List.Split (splitOn)
+import Data.List (nub, sort)
 
 type Description = String
-data Range = Range Description [Integer]
+data Range = Range Description [Int]
 
-type Notes = {
+data Notes = Notes {
     ranges :: [Range],
-    myTicket :: [Integer],
-    nearbyTickets :: [[Integer]]
+    myTicket :: [Int],
+    nearbyTickets :: [[Int]]
 }
 
 input :: String -> Notes
-input filename = {ranges = ranges, myTicket = myTicket, nearbyTickets = nearbyTickets} where
-    processLine l = map read $ split l ","
-    myTicket = processLine $ head $ init $ lines $ toStr $ unsafePerformIO $ readFile (filename ++ "-your")
-    nearbyTickets = map processLine contents where
-        contents = init $ lines $ toStr $ unsafePerformIO $ readFile (filename ++ "-nearby")
-    ranges = map processRange contents where 
-        contents = init $ lines $ toStr $ unsafePerformIO $ readFile (filename ++ "-ranges")
+input filename = Notes {ranges = ranges', myTicket = myTicket', nearbyTickets = nearbyTickets'} where
+    processLine l = map read $ splitOn "," l
+    myTicket' = processLine $ head $ lines $ inputRaw (filename ++ "-your")
+    nearbyTickets' = map processLine $ lines $ inputRaw (filename ++ "-nearby")
+    ranges' = map processRange $ lines $ inputRaw (filename ++ "-ranges") where 
         processRange r = Range desc ([from..to] ++ [from'..to']) where
-            desc = (split r ":") !! 0
-            tokens = split ((split r ":") !! 1) " "
-            (from, to) = (read $ (split (tokens !! 1) "-") !! 0, read $ (split (tokens !! 1) "-") !! 1)
-            (from', to') = (read $ (split (tokens !! 3) "-") !! 0, read $ (split (tokens !! 3) "-") !! 1)
+            desc = (splitOn ":" r) !! 0
+            tokens = splitOn " " ((splitOn ":" r) !! 1)
+            (from, to) = (read $ (splitOn "-" (tokens !! 1)) !! 0, read $ (splitOn "-" (tokens !! 1)) !! 1)
+            (from', to') = (read $ (splitOn "-" (tokens !! 3)) !! 0, read $ (splitOn "-" (tokens !! 3)) !! 1)
 
-part1 :: Notes -> Integer
+part1 :: Notes -> Int
 part1 notes = sum invalid where
-    valid = nub $ sort $ foldl (++) [] $ map (\(Range _ r) -> r) notes.ranges
-    nearby = sort $ foldl (++) [] notes.nearbyTickets
+    valid = nub $ sort $ foldl (++) [] $ map (\(Range _ r) -> r) (ranges notes)
+    nearby = sort $ foldl (++) [] (nearbyTickets notes)
     invalid = filter (\e -> notElem e valid) nearby 
 
-part2 :: Notes -> Integer
-part2 notes = length notes.nearbyTickets 
+part2 :: Notes -> Int
+part2 notes = length (nearbyTickets notes)
