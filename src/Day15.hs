cdeb31b8d63d7e0bb42225f04082018514a03236
Roland Tritsch <roland@tritsch.org>
2021-01-09 10:49:34 +0000

    Day15: Reformat.

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,25 +1,23 @@
-{-|
-Problem: <https://adventofcode.com/2020/day/15>
-
-Solution:
-
-General - Just keep the last time the number was spoken around and
-then look it up when you need to.
-
-Note: For debugging I keep the list of turns a number was spoken around
-and I also build/keep the list of ages. Might need to optimize this for
-part2 (to reduce the mem/stack size). 
-
-Part 1 - Take 2020 turns and return the last number spoken.
-
-Part 2 - ???
--}
+-- |
+-- Problem: <https://adventofcode.com/2020/day/15>
+--
+-- Solution:
+--
+-- General - Just keep the last time the number was spoken around and
+-- then look it up when you need to.
+--
+-- Note: For debugging I keep the list of turns a number was spoken around
+-- and I also build/keep the list of ages. Might need to optimize this for
+-- part2 (to reduce the mem/stack size).
+--
+-- Part 1 - Take 2020 turns and return the last number spoken.
+--
+-- Part 2 - ???
 module Day15 where
-
-import Prelude
 
 import qualified Data.Map as M
 import Util (inputRaw)
+import Prelude
 
 type Spoken = M.Map Int [Int]
 
@@ -31,19 +29,22 @@ speak number turn spoken = M.insertWith (++) number [turn] spoken
 
 whatToSay :: Int -> Int -> Spoken -> Int
 whatToSay number turn spoken
-    | M.notMember number spoken = 0
-    | otherwise = turn - lastTime where
-        lastTime = head $ spoken M.! number
+  | M.notMember number spoken = 0
+  | otherwise = turn - lastTime
+  where
+    lastTime = head $ spoken M.! number
 
 nextTurn :: Int -> Spoken -> Int -> Int -> [Int]
-nextTurn number _  _ 0 = [number] 
-nextTurn number spoken turn turned = nextTurn nextNumber nextSpoken (turn + 1) (turned - 1) ++ [number] where
+nextTurn number _ _ 0 = [number]
+nextTurn number spoken turn turned = nextTurn nextNumber nextSpoken (turn + 1) (turned - 1) ++ [number]
+  where
     nextNumber = whatToSay number turn spoken
     nextSpoken = speak number turn spoken
 
 part1 :: [Int] -> Int
-part1 initial = head $ nextTurn initialNumber initialSpoken initialTurn initialTurned  where
-    initialSpoken = M.fromList $ zip initial [[t]| t <- [1..(length initial)]]
+part1 initial = head $ nextTurn initialNumber initialSpoken initialTurn initialTurned
+  where
+    initialSpoken = M.fromList $ zip initial [[t] | t <- [1 .. (length initial)]]
     initialNumber = whatToSay (last initial) (length initial) initialSpoken
     initialTurn = length initial + 1
     initialTurned = 2020 - length initial - 1
