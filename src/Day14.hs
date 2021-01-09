a8e035ee827e22c973e31d4ed0301fe41e827dd2
Roland Tritsch <roland@tritsch.org>
2021-01-09 08:06:56 +0000

    Day14: Solved.

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,100 +1,98 @@
-{-|
-Problem: <https://adventofcode.com/2020/day/14>
+{-# LANGUAGE RankNTypes #-}
 
-Solution:
-
-General - The crux of this problem is (obviously) to
-figure out the masking.
-
-Key is to realize that the given mask is actually two
-masks: One to set bits and one to unset bits.
-
-You can then mask the given value with ...
-
-band setMask (bor value unsetMask)
-
-Hamler supports bitwise masking on Integers, but
-(AFAI can see it) has only limited support to work
-with BitStrings (hence the additional code below
-to turn a BitString into an Integer).
-
-Part 1 - Just run/fold the instructions over the program
-and when you are done, sum up the memory values.
-
-Part 2 - ???
--}
+-- |
+-- Problem: <https://adventofcode.com/2020/day/14>
+--
+-- Solution:
+--
+-- General - The crux of this problem is (obviously) to
+-- figure out the masking.
+--
+-- Key is to realize that the given mask is actually two
+-- masks: One to set bits and one to unset bits.
+--
+-- You can then mask the given value with ...
+--
+-- band setMask (bor value unsetMask)
+--
+-- Hamler supports bitwise masking on Integers, but
+-- (AFAI can see it) has only limited support to work
+-- with BitStrings (hence the additional code below
+-- to turn a BitString into an Integer).
+--
+-- Part 1 - Just run/fold the instructions over the program
+-- and when you are done, sum up the memory values.
+--
+-- Part 2 - ???
 module Day14 where
 
+import Data.Bits ((.&.), (.|.))
+import qualified Data.Map as M
+import Util (inputRaw)
 import Prelude
 
-import Data.Binary (toStr)
-import Data.Float as F
-import Data.Int (toFloat)
-import Data.List (init, sum, (!!))
-import Data.Map as M
-import Data.String (lines, words, sliceTo)
+type Argument = Int
 
-type Argument = Integer
-type Address = Integer
-type Value = Integer
+type Address = Int
+
+type Value = Int
+
 data Operation = Mask | Write
+  deriving (Eq, Show)
+
 data Instruction = Instruction Operation Argument Argument
-type Program = {
-    counter :: Integer,
-    mask :: (Integer, Integer),
+  deriving (Eq, Show)
+
+data Program = Program
+  { counter :: Int,
+    mask :: (Int, Int),
     memory :: M.Map Address Value
-}
+  }
 
-charToInt0 :: Char -> Integer
-charToInt0 '0' = 0
-charToInt0 '1' = 1
-charToInt0 'X' = 0
-charToInt0 _ = -1
+-- http://pleac.sourceforge.net/pleac_haskell/numbers.html#AEN82
+bin2dec :: String -> Int
+bin2dec = foldr (\c s -> s * 2 + c) 0 . reverse . map c2i
+  where
+    c2i c = if c == '0' then 0 else 1
 
-charToInt1 :: Char -> Integer
-charToInt1 '0' = 0
-charToInt1 '1' = 1
-charToInt1 'X' = 1
-charToInt1 _ = -1
-
-maskStrToBinArray :: (Char -> Integer) -> String -> [Integer]
-maskStrToBinArray f m = map f m
-
-binArrayToInteger :: [Integer] -> Integer
-binArrayToInteger bin = int where
-    aFloat = [toFloat a | a <- reverse bin]
-    bFloat = [toFloat b | b <- [0..((length bin) - 1)]] 
-    binZip = zip aFloat bFloat
-    int = F.trunc $ sum $ map (\(a, b) -> a * F.pow 2.0 b) binZip
+sliceTo :: forall a. Int -> Int -> [a] -> [a]
+sliceTo from to as = take (to - from + 1) (drop from as)
 
 input :: String -> [Instruction]
-input filename = map (\l -> processLine (words l)) contents where
-    contents = init $ lines $ toStr $ unsafePerformIO $ readFile filename
-    processLine ["mask"|args] = Instruction Mask setMask unsetMask where
-        setMask = binArrayToInteger $ maskStrToBinArray charToInt0 (args !! 1)
-        unsetMask = binArrayToInteger $ maskStrToBinArray charToInt1 (args !! 1)
-    processLine tokens = Instruction Write address value where
-        address = read $ sliceTo (tokens !! 0) 4 ((length (tokens !! 0)) - 1)
+input filename = map (\l -> processLine (words l)) contents
+  where
+    contents = lines $ inputRaw filename
+    processLine ("mask" : args) = Instruction Mask setMask unsetMask
+      where
+        setMask = bin2dec $ map (\c -> if c == 'X' then '0' else c) (args !! 1)
+        unsetMask = bin2dec $ map (\c -> if c == 'X' then '1' else c) (args !! 1)
+    processLine tokens = Instruction Write address value
+      where
+        address = read $ sliceTo 4 ((length (tokens !! 0)) - 2) (tokens !! 0)
         value = read (tokens !! 2)
 
 execute :: Program -> Instruction -> Program
-execute program (Instruction Mask setMask unsetMask) = {
-    counter = program.counter + 1,
-    mask = (setMask, unsetMask),
-    memory = program.memory
-}
-execute program (Instruction Write addr value) = {
-    counter = program.counter + 1,
-    mask = program.mask,
-    memory = M.put addr maskedValue program.memory
-} where
-    (setMask, unsetMask) = program.mask
-    maskedValue = band unsetMask (bor value setMask)
-    
-part1 :: [Instruction] -> Integer
-part1 instructions = sum $ M.values done.memory where
-    program = {counter = 0, mask = (0, 0), memory = M.empty}
+execute program (Instruction Mask setMask unsetMask) =
+  Program
+    { counter = counter program + 1,
+      mask = (setMask, unsetMask),
+      memory = memory program
+    }
+execute program (Instruction Write addr value) =
+  Program
+    { counter = counter program + 1,
+      mask = mask program,
+      memory = M.insert addr maskedValue (memory program)
+    }
+  where
+    (setMask, unsetMask) = mask program
+    maskedValue = unsetMask .&. (value .|. setMask)
+
+part1 :: [Instruction] -> Int
+part1 instructions = sum $ M.elems (memory done)
+  where
+    program = Program {counter = 0, mask = (0, 0), memory = M.empty}
     done = foldl execute program instructions
 
-part2 :: [Instruction] -> Integer
+part2 :: [Instruction] -> Int
 part2 instructions = length instructions
