3582b2aabfe6739caf7fb453dbb8f279be306291
Roland Tritsch <roland@tritsch.org>
2021-01-10 17:51:02 +0000

    Day20: Reformat.

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,26 +1,25 @@
-{-|
-Problem: <https://adventofcode.com/2020/day/20>
-
-Solution:
-
-General - ???
-
-Part 1 - ???
-
-Part 2 - ???
--}
+-- |
+-- Problem: <https://adventofcode.com/2020/day/20>
+--
+-- Solution:
+--
+-- General - ???
+--
+-- Part 1 - ???
+--
+-- Part 2 - ???
 module Day20 where
 
+import Util (inputRaw)
 import Prelude
 
-import Util (inputRaw)
-
 input :: String -> [String]
-input filename = contents where
+input filename = contents
+  where
     contents = lines $ inputRaw filename
 
 part1 :: [String] -> Int
-part1 is = length is 
+part1 is = length is
 
 part2 :: [String] -> Int
 part2 is = length is
