82a563e6eb77f594d8f2cdaa0c4cdc3f91ed659a
Roland Tritsch <roland@tritsch.org>
2021-01-16 10:00:37 +0000

    Day25: Part1: Solved.

--- src/Day25.hs
+++ src/Day25.hs
@@ -3,9 +3,16 @@
 --
 -- Solution:
 --
--- General - ???
+-- General - Interesting one. Struggled to read this right and get all of
+-- the numbers/concepts (value, subject, key, ...) right. At least the
+-- parsing of the input is easy ...
 --
--- Part 1 - ???
+-- Main insight was that you can calculate the next value/subject from the
+-- previous one with a loop size of 1. And this will obviously speed up the
+-- brute forcing of the loop size.
+--
+-- Part 1 - Brute force the card loop size and use it to calculate the door
+-- encryption key.
 --
 -- Part 2 - ???
 module Day25 where
@@ -13,7 +20,8 @@ module Day25 where
 import Util (inputRaw)
 import Prelude
 
-type Keys = (Int, Int)
+type Key = Int
+type Keys = (Key, Key)
 
 -- | read the input file.
 input :: String -> Keys
@@ -21,9 +29,33 @@ input filename = (cardPK, doorPK)
   where
     (cardPK : doorPK : _) = map read $ lines $ inputRaw filename
 
+initialValue :: Int
+initialValue = 1
+
+superSecretSubject :: Int
+superSecretSubject = 7
+
+superSecretSeed :: Int
+superSecretSeed = 20201227
+
+-- | transform a subject number.
+transform :: Int -> Int -> Int -> Int
+transform 0 value _subject = value 
+transform loopSize value subject = transform (loopSize - 1) value' subject
+  where
+    value' = mod (value * subject) superSecretSeed
+
+-- | brute force the public key to get the loopSize.
+brute :: Int -> Int -> Key -> Int
+brute loopSize value key
+  | transform 1 value superSecretSubject == key = loopSize
+  | otherwise = brute (loopSize + 1) (transform 1 value superSecretSubject) key
+
 -- | solve part1.
 part1 :: Keys -> Int
-part1 (cardPK, _doorPK) = cardPK
+part1 (cardPK, doorPK) = transform cardLoopSize initialValue doorPK
+  where
+    cardLoopSize = brute 1 initialValue cardPK
 
 -- | solve part2.
 part2 :: Keys -> Int
--- test/Day25Spec.hs
+++ test/Day25Spec.hs
@@ -9,12 +9,26 @@ run = hspec $ do
     it "input" $ do
       (input "./input/Day25p1test.txt") `shouldBe` (5764801, 17807724)
 
+  describe "transform" $ do
+    it "basic" $ do
+      (transform 10 1 123) `shouldBe` 17410834
+    it "door public key" $ do
+      (transform 8 1 17807724) `shouldBe` 14897079
+    it "card public key" $ do
+      (transform 11 1 5764801) `shouldBe` 14897079
+      
+  describe "brute" $ do
+    it "card" $ do
+      (brute 1 1 5764801) `shouldBe` 8
+    it "door" $ do
+      (brute 1 1 17807724) `shouldBe` 11
+
   describe "part1" $ do
     it "testcases" $ do
-      part1 (input "./input/Day25p1test.txt") `shouldBe` 5764801
+      part1 (input "./input/Day25p1test.txt") `shouldBe` 14897079
 
     it "puzzle" $ do
-      part1 (input "./input/Day25p1.txt") `shouldBe` 12578151
+      part1 (input "./input/Day25p1.txt") `shouldBe` 296776
 
   describe "part2" $ do
     it "testcases" $ do
