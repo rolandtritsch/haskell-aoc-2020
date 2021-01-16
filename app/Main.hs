466b9d44c4f7d80b09f06e87a97d31d58ea4fe3e
Roland Tritsch <roland@tritsch.org>
2021-01-16 10:11:47 +0000

    Update main to only run solved solutions.

--- app/Main.hs
+++ app/Main.hs
@@ -15,43 +15,43 @@ import qualified Day05.Part2 as D05P2
 import qualified Day06.Part1 as D06P1
 import qualified Day06.Part2 as D06P2
 import qualified Day07.Part1 as D07P1
-import qualified Day07.Part2 as D07P2
+-- import qualified Day07.Part2 as D07P2
 import qualified Day08.Part1 as D08P1
-import qualified Day08.Part2 as D08P2
+-- import qualified Day08.Part2 as D08P2
 import qualified Day09.Part1 as D09P1
-import qualified Day09.Part2 as D09P2
+-- import qualified Day09.Part2 as D09P2
 import qualified Day10.Part1 as D10P1
-import qualified Day10.Part2 as D10P2
+-- import qualified Day10.Part2 as D10P2
 import qualified Day11.Part1 as D11P1
-import qualified Day11.Part2 as D11P2
+-- import qualified Day11.Part2 as D11P2
 import qualified Day12.Part1 as D12P1
-import qualified Day12.Part2 as D12P2
+-- import qualified Day12.Part2 as D12P2
 import qualified Day13.Part1 as D13P1
-import qualified Day13.Part2 as D13P2
+-- import qualified Day13.Part2 as D13P2
 import qualified Day14.Part1 as D14P1
-import qualified Day14.Part2 as D14P2
+-- import qualified Day14.Part2 as D14P2
 import qualified Day15.Part1 as D15P1
-import qualified Day15.Part2 as D15P2
+-- import qualified Day15.Part2 as D15P2
 import qualified Day16.Part1 as D16P1
-import qualified Day16.Part2 as D16P2
+-- import qualified Day16.Part2 as D16P2
 import qualified Day17.Part1 as D17P1
-import qualified Day17.Part2 as D17P2
+-- import qualified Day17.Part2 as D17P2
 import qualified Day18.Part1 as D18P1
-import qualified Day18.Part2 as D18P2
+-- import qualified Day18.Part2 as D18P2
 import qualified Day19.Part1 as D19P1
-import qualified Day19.Part2 as D19P2
-import qualified Day20.Part1 as D20P1
-import qualified Day20.Part2 as D20P2
+-- import qualified Day19.Part2 as D19P2
+-- import qualified Day20.Part1 as D20P1
+-- import qualified Day20.Part2 as D20P2
 import qualified Day21.Part1 as D21P1
-import qualified Day21.Part2 as D21P2
+-- import qualified Day21.Part2 as D21P2
 import qualified Day22.Part1 as D22P1
-import qualified Day22.Part2 as D22P2
+-- import qualified Day22.Part2 as D22P2
 import qualified Day23.Part1 as D23P1
-import qualified Day23.Part2 as D23P2
+-- import qualified Day23.Part2 as D23P2
 import qualified Day24.Part1 as D24P1
-import qualified Day24.Part2 as D24P2
+-- import qualified Day24.Part2 as D24P2
 import qualified Day25.Part1 as D25P1
-import qualified Day25.Part2 as D25P2
+-- import qualified Day25.Part2 as D25P2
 
 main :: IO ()
 main = do
@@ -70,40 +70,40 @@ main = do
   D06P1.main
   D06P2.main
   D07P1.main
-  D07P2.main
+--   D07P2.main
   D08P1.main
-  D08P2.main
+--   D08P2.main
   D09P1.main
-  D09P2.main
+--   D09P2.main
   D10P1.main
-  D10P2.main
+--   D10P2.main
   D11P1.main
-  D11P2.main
+--   D11P2.main
   D12P1.main
-  D12P2.main
+--   D12P2.main
   D13P1.main
-  D13P2.main
+--   D13P2.main
   D14P1.main
-  D14P2.main
+--   D14P2.main
   D15P1.main
-  D15P2.main
+--   D15P2.main
   D16P1.main
-  D16P2.main
+--   D16P2.main
   D17P1.main
-  D17P2.main
+--   D17P2.main
   D18P1.main
-  D18P2.main
+--   D18P2.main
   D19P1.main
-  D19P2.main
-  D20P1.main
-  D20P2.main
+--   D19P2.main
+--   D20P1.main
+--   D20P2.main
   D21P1.main
-  D21P2.main
+--   D21P2.main
   D22P1.main
-  D22P2.main
+--   D22P2.main
   D23P1.main
-  D23P2.main
+--   D23P2.main
   D24P1.main
-  D24P2.main
+--   D24P2.main
   D25P1.main
-  D25P2.main
+--   D25P2.main
