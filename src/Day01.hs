43e4e95029a1c1866bd8c147b05ea8cd26e1649a
Roland Tritsch <roland@tritsch.org>
2021-06-04 10:34:00 +0100

    Day01: Refactor. Make it more efficient (and more correct :)).

--- src/Day01.hs
+++ src/Day01.hs
@@ -3,17 +3,28 @@
 --
 -- Solution:
 --
--- General - List combination/search problem. Build all combination
--- of list elements. And lock for the ones that sum up to be 2020.
+-- General - List combination/search problem. Build all combinations
+-- of list elements. And look for the one that sums up to be 2020.
+--
+-- Given that '+' is commutative, we only need to build [(1,2), (1,3),
+-- (2,3)] out of [1, 2, 3]. And we can stop looking as soon as we find
+-- the first tuple that satisfies the condition (making the evaluation
+-- of the list (of tuples) lazy (build only the part of the list you
+-- need to find the first matching tuple) is another optimisation to
+-- consider).
+--
+-- Another nice variation on the challenge would be to think about
+-- how to implement combinationsN. But that is for another day ...
 --
 -- Part 1 - Do it with pairs.
 --
--- Part 2 - Do it with triplets. Note: To make the search space small,
--- we first search the expenses in reverse order and then stop searching,
--- when the first 2 numbers are already bigger than 2020.
+-- Part 2 - Do it with triplets.
+
 module Day01 where
 
 import Util (inputRaw)
+import Data.List (find)
+import Data.Maybe (fromJust)
 
 type Expense = Int
 
@@ -21,12 +32,24 @@ type Expense = Int
 input :: String -> [Expense]
 input = map read . lines . inputRaw
 
+combinations2 :: [a] -> [(a, a)]
+combinations2 [] = []
+combinations2 (e:es) = [(e, e') | e' <- es] ++ combinations2 es
+
+combinations3 :: [a] -> [(a, a, a)]
+combinations3 [] = []
+combinations3 (e:es) = [(e, e', e'') | (e', e'') <- combinations2 es] ++ combinations3 es
+
 part1 :: [Expense] -> Int
 part1 expenses = x * y
   where
-    (x, y) = head $ [(x', y') | x' <- expenses, y' <- expenses, x' + y' == 2020]
+    (x, y) = fromJust
+      $ find (\(x', y') -> x' + y' == 2020)
+      $ combinations2 expenses
 
 part2 :: [Expense] -> Int
 part2 expenses = x * y * z
   where
-    (x, y, z) = head $ [(x', y', z') | x' <- expenses, y' <- expenses, x' + y' <= 2020, z' <- expenses, x' + y' + z' == 2020]
+    (x, y, z) = fromJust
+      $ find (\(x', y', z') -> x' + y' + z' == 2020)
+      $ combinations3 expenses
--- test/Day01Spec.hs
+++ test/Day01Spec.hs
@@ -10,6 +10,28 @@ run = hspec $ do
     it "input" $ do
       input "./input/Day01p1test.txt" `shouldBe` [1721, 979, 366, 299, 675, 1456] 
 
+  describe "combinations2" $ do
+    it "[1,2,3]" $ do
+      let expected :: [(Expense, Expense)]
+          expected = [(1,2), (1,3), (2,3)]
+      combinations2 [1, 2, 3] `shouldBe` expected
+
+    it "testcase" $ do
+      let expected :: [(Expense, Expense)]
+          expected = [(1721,979),(1721,366),(1721,299),(1721,675),(1721,1456),(979,366),(979,299),(979,675),(979,1456),(366,299),(366,675),(366,1456),(299,675),(299,1456),(675,1456)]
+      (combinations2 . input) "./input/Day01p1test.txt" `shouldBe` expected
+
+  describe "combinations3" $ do
+    it "[1,2,3,4]" $ do
+      let expected :: [(Expense, Expense, Expense)]
+          expected = [(1,2,3), (1,2,4), (1,3,4), (2,3,4)]
+      combinations3 [1, 2, 3, 4] `shouldBe` expected
+
+    it "testcase" $ do
+      let expected :: [(Expense, Expense, Expense)]
+          expected = [(1721,979,366),(1721,979,299),(1721,979,675),(1721,979,1456),(1721,366,299),(1721,366,675),(1721,366,1456),(1721,299,675),(1721,299,1456),(1721,675,1456),(979,366,299),(979,366,675),(979,366,1456),(979,299,675),(979,299,1456),(979,675,1456),(366,299,675),(366,299,1456),(366,675,1456),(299,675,1456)]
+      (combinations3 . input) "./input/Day01p1test.txt" `shouldBe` expected
+
   describe "part1" $ do
     it "testcase" $ do
       (part1 . input) "./input/Day01p1test.txt" `shouldBe` 514579
