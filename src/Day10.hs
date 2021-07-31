c859ede750eaa939a8bd9ed78fbae8de55b4889f
Roland Tritsch <roland@tritsch.org>
2021-07-31 15:03:18 +0100

    Day10: Update/refesh doc.

--- src/Day10.hs
+++ src/Day10.hs
@@ -3,12 +3,13 @@
 --
 -- Solution:
 --
--- General - Connect jolts/volts. The trick here is to read the problem
--- description carefully. You then realize that you can solve the problem
--- by building the diffs between 2 consecutive numbers and just count the
--- number of 1 and 3 diffs.
+-- General - Connect jolts/volts.
 --
--- Part 1 - Multiply the number of ones and threes.
+-- Part 1 - The trick here is to read the problem
+-- description carefully. You then realize that you
+-- can solve the problem by building the diffs between
+-- two consecutive numbers and just count the number of
+-- 1 and 3 diffs. Multiply the number of 1s and 3s.
 --
 -- Part 2 - ???
 module Day10 where
