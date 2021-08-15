97d7fc1094448971817489b9456bd49adf1813a7
Roland Tritsch <roland@tritsch.org>
2021-08-15 07:19:21 +0100

    Day13: Part2: Done.

--- src/Day13.hs
+++ src/Day13.hs
@@ -12,25 +12,72 @@
 --
 -- Part 1 - Find the minimum and calculate the solution.
 --
--- Part 2 - ???
+-- Part 2 - We have a system of equations along the lines of ...
+--
+-- mod x b0 = 0
+-- mod x+1 b1 = 0
+-- ...
+-- mod x+n bn = 0
+--
+-- We are looking for the smallest x to make this system work.
+--
+-- The first idea would be to try all numbers until all equations
+-- work, but ... this will take a long time. An optimization would
+-- be to only try the numbers that we get from the first equation.
+--
+-- The next optimisation is to pick the equation that is most
+-- selective (the max of all bus ids).
+--
+-- But (not too surprsingly) this works for the testcases, but not
+-- to solve the problem.
+--
+-- Next step is to realize that the system above can be solved
+-- using the Chinese Remainder Algorithm. But for that we need
+-- to restructure the system to ...
+--
+-- mod x b0 = b0-0
+-- mod x b1 = b1-1
+-- ...
+-- mod x bn = bn-n
+--
+-- (Conveniently) The input for the chineseRemainder is the
+-- list of [(b0, b0-0), (b1, b1-1), ...] pairs.
+--
+-- But then you realize that you found a solution for the system,
+-- but not the smallest one. You can get the smallest solution
+-- by caluculating the mod of the chineseRemainder and the product
+-- of all moduli. 
+--
+-- That's it.
 
 module Day13 where
 
+import Numeric.Domain.Euclidean (chineseRemainder)
+
+import Data.List (sortOn, find)
 import Data.List.Split (splitOn)
+import Data.Maybe (fromJust)
 import Util (inputRaw)
 import Prelude
 
 data Schedule = Schedule
   { departure :: Int,
-    busses :: [Int]
+    busses :: [Int],
+    busses' :: [(Integer, Integer)]    
   }
 
 input :: String -> Schedule
-input filename = Schedule {departure = departure', busses = busses'}
+input filename = Schedule {departure = departure', busses = busses'', busses' = busses'''}
   where
     contents = lines $ inputRaw filename
     departure' = read $ head contents
-    busses' = map read $ filter (/= "x") $ splitOn "," (contents !! 1)
+    busses'' = map read $ filter (/= "x") $ splitOn "," (contents !! 1)
+    busses''' = map f $ filter (\(b, _) -> b /= "x") $ zip (splitOn "," (contents !! 1)) [0..]
+      where
+        f (b, o) = (b', o')
+          where
+            b' = read b :: Integer 
+            o' = b' - o :: Integer
 
 part1 :: Schedule -> Int
 part1 schedule = minutes * bus
@@ -39,5 +86,23 @@ part1 schedule = minutes * bus
       where
         waitTime b = ((negate $ mod (departure schedule) b) + b, b)
 
-part2 :: Schedule -> Int
-part2 schedule = length (busses schedule)
+part2' :: Schedule -> Integer
+part2' schedule = (fromJust $ find solve possible) - bn
+  where
+    bs = reverse $ sortOn fst $ map f (busses' schedule)
+      where
+        f (b, o) = (b, b - o)
+    bn = snd $ head bs
+    possible = map toInteger [mb, mb * 2 ..]
+      where
+        mb = fst $ maximum (busses' schedule)
+    solve p = all check bs
+      where
+        check (b, o) = mod (p+o-bn) b == 0
+        
+part2 :: Schedule -> Integer
+part2 schedule = mod cr m
+  where
+    bs = busses' schedule
+    cr = chineseRemainder bs
+    m = product $ map fst bs
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -10,6 +10,8 @@ run = hspec $ do
       let schedule = input "./input/Day13p1test.txt"
       departure schedule `shouldBe` 939
       length (busses schedule) `shouldBe` 5
+      (busses schedule) `shouldBe` [7,13,59,31,19]
+      (busses' schedule) `shouldBe` [(7,7),(13,12),(59,55),(31,25),(19,12)]
 
   describe "part1" $ do
     it "testcases" $ do
@@ -18,9 +20,24 @@ run = hspec $ do
     it "puzzle" $ do
       part1 (input "./input/Day13p1.txt") `shouldBe` 2305
 
+  describe "part2'" $ do
+    it "testcases" $ do
+      part2' (Schedule {departure = 0, busses = [], busses' = [(17,17),(13,11),(19,16)]}) `shouldBe` 3417
+      part2' (Schedule {departure = 0, busses = [], busses' = [(67, 67),(7,6),(59,57),(61,58)]}) `shouldBe` 754018
+      part2' (Schedule {departure = 0, busses = [], busses' = [(67,67),(7,5),(59,56),(61,57)]}) `shouldBe` 779210
+      part2' (Schedule {departure = 0, busses = [], busses' = [(67,67),(7,6),(59,56),(61,57)]}) `shouldBe` 1261476
+      part2' (Schedule {departure = 0, busses = [], busses' = [(1789,1789),(37,36),(47,45),(1889,1886)]}) `shouldBe` 1202161486 
+      part2' (input "./input/Day13p1test.txt") `shouldBe` 1068781
+      
   describe "part2" $ do
     it "testcases" $ do
-      part2 (input "./input/Day13p1test.txt") `shouldBe` 5
-
+      part2 (Schedule {departure = 0, busses = [], busses' = [(17,17),(13,11),(19,16)]}) `shouldBe` 3417
+      part2 (Schedule {departure = 0, busses = [], busses' = [(67,67),(7,6),(59,57),(61,58)]}) `shouldBe` 754018
+      part2 (Schedule {departure = 0, busses = [], busses' = [(67,67),(7,5),(59,56),(61,57)]}) `shouldBe` 779210
+      part2 (Schedule {departure = 0, busses = [], busses' = [(67,67),(7,6),(59,56),(61,57)]}) `shouldBe` 1261476
+      part2 (Schedule {departure = 0, busses = [], busses' = [(1789,1789),(37,36),(47,45),(1889,1886)]}) `shouldBe` 1202161486 
+      part2 (input "./input/Day13p1test.txt") `shouldBe` 1068781
+      
     it "puzzle" $ do
-      part2 (input "./input/Day13p1.txt") `shouldBe` 9
+      part2 (input "./input/Day13p1.txt") `shouldBe` 552612234243498
+      
