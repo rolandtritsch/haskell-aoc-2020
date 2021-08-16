29a4b59377a777d17c9145136988c4f04ec6f94a
Roland Tritsch <roland@tritsch.org>
2021-08-16 10:09:04 +0100

    Day14: Part2: Done.

--- src/Day14.hs
+++ src/Day14.hs
@@ -20,13 +20,23 @@
 -- with BitStrings (hence the additional code below
 -- to turn a BitString into an Integer).
 --
--- Part 1 - Just run/fold the instructions over the program
+-- Part 1 - Just run/fold the operations over the program
 -- and when you are done, sum up the memory values.
 --
--- Part 2 - ???
+-- Part 2 - Initially this sounded/looked easy: Do the same
+-- thing again, but with a different implementation for the
+-- execution of the operations.
+--
+-- But then I as not able to find a way to implement part2
+-- with a bitwise mask and had to go back to string manipulation
+-- to implement the first masking operation (applyMask).
+--
+-- Not elegant, but it works.
 
 module Day14 where
 
+import Numeric (showIntAtBase)
+import Data.Char (intToDigit)
 import Data.Bits ((.&.), (.|.))
 import qualified Data.Map as M
 import Util (inputRaw)
@@ -36,13 +46,13 @@ type Address = Int
 
 type Value = Int
 
-data Operation = Mask Int Int [Int] | Write Int Int
+data Operation = Mask Int Int String | Write Int Int
   deriving (Eq, Show)
 
 data Program = Program
   { counter :: Int,
     mask :: (Int, Int),
-    memMasks :: [Int],
+    memMask :: String,
     memory :: M.Map Address Value
   }
 
@@ -55,28 +65,15 @@ bin2dec = foldr (\c s -> s * 2 + c) 0 . reverse . map c2i
 sliceTo :: forall a. Int -> Int -> [a] -> [a]
 sliceTo from to as = take (to - from + 1) (drop from as)
 
-buildMasks :: String -> [String]
-buildMasks (m:ms) = go m ms [] []
-  where
-    go '0' [] s ss = ss ++ [s ++ "0"]
-    go '1' [] s ss = ss ++ [s ++ "1"]
-    go 'X' [] s ss = ss ++ [s ++ "0"] ++ [s ++ "1"]
-    go '0' (m':ms') s ss = go m' ms' (s ++ "0") ss 
-    go '1' (m':ms') s ss = go m' ms' (s ++ "1") ss 
-    go 'X' (m':ms') s ss = (go m' ms' (s ++ "0") ss) ++ (go m' ms' (s ++ "1") ss)
-    go _ _ _ _ = error "buildMasks - go: Unexpected pattern match"
-buildMasks [] = []
-
 input :: String -> [Operation]
 input filename = map (\l -> processLine (words l)) contents
   where
     contents = lines $ inputRaw filename
-    processLine ("mask" : args) = Mask setMask unsetMask memMasks'
+    processLine ("mask" : args) = Mask setMask unsetMask mask'
       where
         mask' = args !! 1
         setMask = bin2dec $ map (\c -> if c == 'X' then '0' else c) mask'
         unsetMask = bin2dec $ map (\c -> if c == 'X' then '1' else c) mask'
-        memMasks' = map bin2dec $ buildMasks mask'
     processLine tokens = Write address value
       where
         address = read $ sliceTo 4 ((length (tokens !! 0)) - 2) (tokens !! 0)
@@ -87,14 +84,14 @@ execute program (Mask setMask unsetMask _) =
   Program
     { counter = counter program + 1,
       mask = (setMask, unsetMask),
-      memMasks = [],
+      memMask = [],
       memory = memory program
     }
 execute program (Write addr value) =
   Program
     { counter = counter program + 1,
       mask = mask program,
-      memMasks = [],
+      memMask = [],
       memory = M.insert addr maskedValue (memory program)
     }
   where
@@ -104,29 +101,58 @@ execute program (Write addr value) =
 part1 :: [Operation] -> Int
 part1 operations = sum $ M.elems (memory done)
   where
-    program = Program {counter = 0, mask = (0,0), memMasks = [], memory = M.empty}
+    program = Program {counter = 0, mask = (0,0), memMask = [], memory = M.empty}
     done = foldl execute program operations
 
--- execute' :: Program -> Operation -> Program
--- execute' program (Mask setMask unsetMask memMasks) =
---   Program
---     { counter = counter program + 1,
---       mask = (setMask, unsetMask),
---       memMasks = memMasks,
---       memory = memory program
---     }
--- execute' program (Write addr value) =
---   Program
---     { counter = counter program + 1,
---       mask = mask program,
---       memMasks = memMasks program,
---       memory = updateMemory (memMasks program) (memory program)
---     }
---   where
---     updateMemory memMasks memory = memory
+dec2bin :: Int -> String
+dec2bin n = showIntAtBase 2 intToDigit n ""
 
--- part2 :: [Operation] -> Int
--- part2 operations = sum $ M.elems (memory done)
---   where
---     program = Program {counter = 0, mask = (0,0), memMasks = [], memory = M.empty}
---     done = foldl execute' program operations 
+prepend :: Char -> Int -> String -> String
+prepend c l s
+  | l == length s = s
+  | otherwise = prepend c l (c : s)
+
+applyMask :: String -> String -> String
+applyMask addr' mask' = go addr'' mask''
+  where
+    l = max (length addr') (length mask')
+    addr'' = prepend '0' l addr' 
+    mask'' = prepend '0' l mask'
+    go (a:as) (m:ms)
+      | m == 'X' = 'X' : go as ms
+      | m == '1' = '1' : go as ms
+      | otherwise = a : go as ms
+    go _ _ = []
+
+buildMasks :: String -> [String]
+buildMasks [] = [""]
+buildMasks (d:ds)
+    | d == 'X' = map ('0':) (buildMasks ds) ++ map ('1':) (buildMasks ds)
+    | otherwise = map (d:) (buildMasks ds)
+
+execute' :: Program -> Operation -> Program
+execute' program (Mask _ _ memMask') =
+  Program
+    { counter = counter program + 1,
+      mask = (0,0),
+      memMask = memMask',
+      memory = memory program
+    }
+execute' program (Write addr value) =
+  Program
+    { counter = counter program + 1,
+      mask = (0,0),
+      memMask = memMask program,
+      memory = updateMemory addr value (memory program) (memMask program)
+    }
+  where
+    updateMemory addr' value' memory' memMask'  = foldl update memory' addresses
+      where
+        addresses = map bin2dec $ buildMasks $ applyMask (dec2bin addr') memMask'
+        update m a = M.insert a value' m  
+
+part2 :: [Operation] -> Int
+part2 operations = sum $ M.elems (memory done)
+  where
+    program = Program {counter = 0, mask = (0,0), memMask = [], memory = M.empty}
+    done = foldl execute' program operations
--- test/Day14Spec.hs
+++ test/Day14Spec.hs
@@ -31,6 +31,20 @@ run = hspec $ do
     it "puzzle" $ do
       part1 (input "./input/Day14p1.txt") `shouldBe` 7440382076205
 
+  describe "dec2bin" $ do
+    it "testcases" $ do
+      dec2bin 42 `shouldBe` "101010" 
+      dec2bin 26 `shouldBe` "11010" 
+
+  describe "prepend" $ do
+    it "testcases" $ do
+      prepend '0' 4 "10" `shouldBe` "0010" 
+
+  describe "applyMask" $ do
+    it "testcases" $ do
+      applyMask "101010" "X1001X" `shouldBe` "X1101X" 
+      applyMask "11010" "0X0XX" `shouldBe` "1X0XX" 
+
   describe "buildMasks" $ do
     it "testcases" $ do
       let expected =
@@ -42,9 +56,28 @@ run = hspec $ do
             ]              
       buildMasks "10XX" `shouldBe` expected
 
-  -- describe "part2" $ do
-  --   it "testcases" $ do
-  --     part2 (input "./input/Day14p1test.txt") `shouldBe` 208
+      let expected' =
+            [
+              "010010",
+              "010011",
+              "110010",
+              "110011"
+            ]              
+      buildMasks "X1001X" `shouldBe` expected'
 
-  --   it "puzzle" $ do
-  --     part2 (input "./input/Day14p1.txt") `shouldBe` 0
+  describe "memMasks" $ do
+    it "testcases" $ do
+      let expected = [26,27,58,59]
+      let as = map bin2dec $ buildMasks $ applyMask (dec2bin 42) "X1001X"
+      as `shouldBe` expected
+
+      let expected' = [16,17,18,19,24,25,26,27]
+      let as' = map bin2dec $ buildMasks $ applyMask (dec2bin 26) "0X0XX"
+      as' `shouldBe` expected'
+
+  describe "part2" $ do
+    it "testcases" $ do
+      part2 (input "./input/Day14p2test.txt") `shouldBe` 208
+
+    it "puzzle" $ do
+      part2 (input "./input/Day14p1.txt") `shouldBe` 4200656704538
