f8b93e539f17198f23eb921cb34686ebd3cd9fe6
Roland Tritsch <roland@tritsch.org>
2021-08-23 21:10:34 +0100

    Day16: Part2: invalidFields and validTickets.

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,50 +1,68 @@
-{-|
-Problem: <https://adventofcode.com/2020/day/16>
-
-Solution:
-
-General - Main problem here is that the input file is difficult
-to parse. I broke it up into 3 files (-ranges, -your, -nearby)
-and parse these files seperately.
-
-Part 1 - After that we just have to filter out the invalid codes
-from all codes (given by the nearby tickes.
-
-Part 2 - ???
--}
+-- |
+-- Problem: <https://adventofcode.com/2020/day/16>
+--
+-- Solution:
+--
+-- General - Main problem here is that the input file is difficult
+-- to parse. I broke it up into 3 files (-ranges, -your, -nearby)
+-- and parse these files seperately.
+--
+-- Part 1 - After that we just have to filter out the invalid codes
+-- from all codes (given by the nearby tickes).
+--
+-- Part 2 - ???
 module Day16 where
 
-import Prelude
-import Util (inputRaw)
-import Data.List.Split (splitOn)
 import Data.List (nub, sort)
+import Data.List.Split (splitOn)
+import Util (inputRaw)
+import Prelude
 
 type Description = String
-data Range = Range Description [Int]
 
-data Notes = Notes {
-    ranges :: [Range],
-    myTicket :: [Int],
-    nearbyTickets :: [[Int]]
-}
+type Field = Int
 
+data Range = Range Description [Field]
+  deriving (Eq, Show)
+
+data Notes = Notes
+  { ranges :: [Range],
+    myTicket :: [Field],
+    nearbyTickets :: [[Field]]
+  }
+  deriving (Eq, Show)
+
+-- | returns the parsed notes
 input :: String -> Notes
-input filename = Notes {ranges = ranges', myTicket = myTicket', nearbyTickets = nearbyTickets'} where
+input filename = Notes {ranges = ranges', myTicket = myTicket', nearbyTickets = nearbyTickets'}
+  where
     processLine l = map read $ splitOn "," l
     myTicket' = processLine $ head $ lines $ inputRaw (filename ++ "-your")
     nearbyTickets' = map processLine $ lines $ inputRaw (filename ++ "-nearby")
-    ranges' = map processRange $ lines $ inputRaw (filename ++ "-ranges") where 
-        processRange r = Range desc ([from..to] ++ [from'..to']) where
+    ranges' = map processRange $ lines $ inputRaw (filename ++ "-ranges")
+      where
+        processRange r = Range desc ([from .. to] ++ [from' .. to'])
+          where
             desc = (splitOn ":" r) !! 0
             tokens = splitOn " " ((splitOn ":" r) !! 1)
             (from, to) = (read $ (splitOn "-" (tokens !! 1)) !! 0, read $ (splitOn "-" (tokens !! 1)) !! 1)
             (from', to') = (read $ (splitOn "-" (tokens !! 3)) !! 0, read $ (splitOn "-" (tokens !! 3)) !! 1)
 
-part1 :: Notes -> Int
-part1 notes = sum invalid where
+-- | returns invalid fields
+invalidFields :: Notes -> [Field] 
+invalidFields notes = filter (\e -> notElem e valid) nearby
+  where
     valid = nub $ sort $ foldl (++) [] $ map (\(Range _ r) -> r) (ranges notes)
     nearby = sort $ foldl (++) [] (nearbyTickets notes)
-    invalid = filter (\e -> notElem e valid) nearby 
+
+-- | returns valid tickets
+validTickets :: [Field] -> [[Field]] -> [[Field]]
+validTickets invalid nearby = filter (not . isInValid) nearby
+  where
+    isInValid = any $ flip elem invalid
+
+part1 :: Notes -> Int
+part1 notes = sum $ invalidFields notes
 
 part2 :: Notes -> Int
 part2 notes = length (nearbyTickets notes)
--- test/Day16Spec.hs
+++ test/Day16Spec.hs
@@ -23,5 +23,21 @@ run = hspec $ do
     it "testcases" $ do
       part2 (input "./input/Day16p1test.txt") `shouldBe` 4
 
+  describe "invalidFields" $ do
+    it "testcases" $ do
+      invalidFields (input "./input/Day16p1test.txt") `shouldBe` [4,12,55]
+
+  describe "validTickets" $ do
+    it "testcases" $ do
+      let notes = input "./input/Day16p1test.txt"
+      validTickets (invalidFields notes) (nearbyTickets notes) `shouldBe` [[7,3,47]]
+      
+      let notes' = input "./input/Day16p2test.txt"
+      validTickets (invalidFields notes') (nearbyTickets notes') `shouldBe` [[3,9,18],[15,1,5],[5,14,9]]
+
+  describe "part2" $ do
+    it "testcases" $ do
+      part2 (input "./input/Day16p1test.txt") `shouldBe` 4
+
     it "puzzle" $ do
       part2 (input "./input/Day16p1.txt") `shouldBe` 236
