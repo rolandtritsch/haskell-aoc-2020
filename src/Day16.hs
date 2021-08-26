a17eb3586a302b4a8cf8476fe261a21d0c332a3b
Roland Tritsch <roland@tritsch.org>
2021-08-26 18:07:45 +0100

    Day16: Part2: Done (needs a cleanup/refactor).

--- src/Day16.hs
+++ src/Day16.hs
@@ -13,8 +13,9 @@
 -- Part 2 - ???
 module Day16 where
 
-import Data.List (nub)
+import Data.List (find, findIndex, isInfixOf, nub, transpose)
 import Data.List.Split (splitOn)
+import Data.Maybe (fromJust)
 import qualified Data.Map as M
 
 import Util (inputRaw)
@@ -57,14 +58,68 @@ invalidFields notes = filter (\e -> notElem e valid) nearby
     valid = nub $ concat $ M.elems $ ranges notes
     nearby = concat $ nearbyTickets notes
 
+-- | solving part1
+part1 :: Notes -> Int
+part1 notes = sum $ invalidFields notes
+
 -- | returns valid tickets
 validTickets :: [Field] -> [[Field]] -> [[Field]]
 validTickets invalid nearby = filter (not . isInValid) nearby
   where
     isInValid = any $ flip elem invalid
 
-part1 :: Notes -> Int
-part1 notes = sum $ invalidFields notes
+-- | returns index of given range desc
+rangeIndex :: [[Field]] -> [Field] -> Int
+rangeIndex valid fields = fromJust $ findIndex range (transpose valid)
+  where
+    range = all (flip elem fields)
 
+-- | returns the range indexes for ranges that have the given word in it
+rangeIndexes :: String -> Ranges -> [[Field]] -> [Int]
+rangeIndexes word ranges' valid = map (rangeIndex valid) fields
+  where
+    fields = map ((M.!) ranges') $ filter (isInfixOf word) (M.keys ranges')
+
+-- reduce :: [[Field]] -> Ranges -> ((String, Int), [[Field]])
+-- reduce valid ranges = ((desc, col), next)
+--   where
+--     cols = zip [0..] $ transpose valid
+
+-- | for a given range, check which cols are valid
+check :: [Field] -> [(Int, [Field])] -> [(Int, Bool)]
+check fields cols = map (\(c, fs) -> (c, all (flip elem fields) fs)) cols
+
+-- | for all ranges, check which cols are valid
+check' :: [(Description, [Field])] -> [(Int, [Field])] -> [(Description, [(Int, Bool)])]
+check' ranges' cols = map (\(desc, fields) -> (desc, check fields cols)) ranges'
+
+-- | find the (range, col) that is unique
+find' :: [(Description, [(Int, Bool)])] -> (Description, Int)
+find' checks = (desc, col)
+  where
+    (desc, cols) = fromJust $ find unique checks
+    unique (_, cs) = (length $ filter snd cs) == 1
+    (col, _) = fromJust $ find snd cols
+
+-- | (recursively) collect all of the col indexes
+collect :: Ranges -> [(Int, [Field])] -> [(Description, Int)] -> [(Description, Int)]
+collect ranges' cols' fields'
+  | M.size ranges' == 0 = fields'
+  | otherwise = collect ranges'' cols'' fields''
+  where
+    next@(desc, col) = find' $ check' (M.toList ranges') cols'
+    ranges'' = M.delete desc ranges'
+    cols'' = filter ((/=) col . fst) cols'
+    fields'' = next : fields'
+
+-- | returns the fields for the word
+solve :: String -> Notes -> [Int]
+solve word notes = map ((!!) $ myTicket notes) cidx
+   where
+     valid = validTickets (invalidFields notes) (nearbyTickets notes)
+     cols = zip [0..] $ transpose valid
+     cidx = map snd $ filter (isInfixOf word . fst) $ collect (ranges notes) cols []
+
+-- | solves part2
 part2 :: Notes -> Int
-part2 notes = length (nearbyTickets notes)
+part2 notes = product $ solve "departure" notes
--- test/Day16Spec.hs
+++ test/Day16Spec.hs
@@ -1,5 +1,8 @@
 module Day16Spec where
 
+import qualified Data.Map as M
+import Data.List (transpose)
+
 import Day16
 import Test.Hspec
 
@@ -12,20 +15,16 @@ run = hspec $ do
       length (myTicket program) `shouldBe` 3
       length (nearbyTickets program) `shouldBe` 4 
 
+  describe "invalidFields" $ do
+    it "testcases" $ do
+      invalidFields (input "./input/Day16p1test.txt") `shouldBe` [4,55,12]
+
   describe "part1" $ do
     it "testcases" $ do
       part1 (input "./input/Day16p1test.txt") `shouldBe` 71
 
     it "puzzle" $ do
       part1 (input "./input/Day16p1.txt") `shouldBe` 21071
-
-  describe "part2" $ do
-    it "testcases" $ do
-      part2 (input "./input/Day16p1test.txt") `shouldBe` 4
-
-  describe "invalidFields" $ do
-    it "testcases" $ do
-      invalidFields (input "./input/Day16p1test.txt") `shouldBe` [4,55,12]
 
   describe "validTickets" $ do
     it "testcases" $ do
@@ -35,9 +34,107 @@ run = hspec $ do
       let notes' = input "./input/Day16p2test.txt"
       validTickets (invalidFields notes') (nearbyTickets notes') `shouldBe` [[3,9,18],[15,1,5],[5,14,9]]
 
-  describe "part2" $ do
-    it "testcases" $ do
-      part2 (input "./input/Day16p1test.txt") `shouldBe` 4
+  describe "rangeIndex" $ do
+    it "testcase - row" $ do
+      let notes = input "./input/Day16p2test.txt"
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let fields = (M.!) (ranges notes) "row"
+      rangeIndex valid fields `shouldBe` 0
+      (!!) (myTicket notes) (rangeIndex valid fields) `shouldBe` 11 
 
+    it "testcase - class" $ do
+      let notes = input "./input/Day16p2test.txt"
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let fields = (M.!) (ranges notes) "class"
+      rangeIndex valid fields `shouldBe` 1
+      (!!) (myTicket notes) (rangeIndex valid fields) `shouldBe` 12 
+
+    it "testcase - seat" $ do
+      let notes = input "./input/Day16p2test.txt"
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let fields = (M.!) (ranges notes) "seat"
+      rangeIndex valid fields `shouldBe` 2
+      (!!) (myTicket notes) (rangeIndex valid fields) `shouldBe` 13 
+
+    -- it "puzzle - departure date" $ do
+    --   let notes = input "./input/Day16p1.txt"
+    --   let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+    --   let fields = (M.!) (ranges notes) "departure date"
+    --   rangeIndex valid fields `shouldBe` 14
+    --   (!!) (myTicket notes) (rangeIndex valid fields) `shouldBe` 53
+
+    -- it "puzzle - departure time" $ do
+    --   let notes = input "./input/Day16p1.txt"
+    --   let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+    --   let fields = (M.!) (ranges notes) "departure date"
+    --   rangeIndex valid fields `shouldBe` 2
+    --   (!!) (myTicket notes) (rangeIndex valid fields) `shouldBe` 223
+
+  describe "check" $ do
+    it "testcase" $ do
+      let notes = input "./input/Day16p2test.txt"
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let fields = (M.!) (ranges notes) "seat"
+      let cols = zip [0..] $ transpose valid
+      (length $ filter snd $ check fields cols) `shouldBe` 1
+      
     it "puzzle" $ do
-      part2 (input "./input/Day16p1.txt") `shouldBe` 236
+      let notes = input "./input/Day16p1.txt"
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let fields = (M.!) (ranges notes) "arrival location"
+      let cols = zip [0..] $ transpose valid
+      (length $ filter snd $ check fields cols) `shouldBe` 1
+      
+  describe "find" $ do
+    it "testcase - first" $ do
+      let notes = input "./input/Day16p2test.txt"
+      let ranges' = M.toList $ ranges notes
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let cols = zip [0..] $ transpose valid
+      (find' $ check' ranges' cols) `shouldBe` ("seat", 2)
+      
+    it "testcase - second" $ do
+      let notes = input "./input/Day16p2test.txt"
+      let ranges' = M.toList $ M.delete "seat" $ ranges notes
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let cols = zip [0..] $ transpose valid
+      let cols' = take 2 cols
+      (find' $ check' ranges' cols') `shouldBe` ("class", 1)
+      
+    it "puzzle - first" $ do
+      let notes = input "./input/Day16p1.txt"
+      let ranges' = M.toList $ ranges notes
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let cols = zip [0..] $ transpose valid
+      (find' $ check' ranges' cols) `shouldBe` ("arrival location", 9)
+      
+    it "puzzle - second" $ do
+      let notes = input "./input/Day16p1.txt"
+      let ranges' = M.toList $ M.delete "arrival location" $ ranges notes
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let cols = zip [0..] $ transpose valid
+      let cols' = (take 9 cols) ++ (drop 10 cols)
+      (find' $ check' ranges' cols') `shouldBe` ("class", 0)
+      
+  describe "collect" $ do
+    it "testcase" $ do
+      let notes = input "./input/Day16p2test.txt"
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let cols = zip [0..] $ transpose valid
+      (collect (ranges notes) cols []) `shouldBe` [("row",0),("class",1),("seat",2)]
+      
+    it "puzzle" $ do
+      let notes = input "./input/Day16p1.txt"
+      let valid = validTickets (invalidFields notes) (nearbyTickets notes)
+      let cols = zip [0..] $ transpose valid
+      (collect (ranges notes) cols []) `shouldBe` [("price",4),("duration",15),("arrival track",13),("row",1),("departure platform",18),("departure track",5),("departure date",14),("departure station",8),("departure location",10),("departure time",2),("arrival platform",17),("route",12),("zone",6),("arrival station",11),("seat",19),("type",3),("wagon",7),("train",16),("class",0),("arrival location",9)]
+      
+  describe "solve" $ do
+    it "testcase" $ do
+      solve "" (input "./input/Day16p2test.txt") `shouldBe` [11,12,13]
+      
+    it "puzzle" $ do
+      solve "departure" (input "./input/Day16p1.txt") `shouldBe` [83,227,53,73,211,223]
+  describe "part2" $ do
+    it "puzzle" $ do
+      part2 (input "./input/Day16p1.txt") `shouldBe` 3429967441937
