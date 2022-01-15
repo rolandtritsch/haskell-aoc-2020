abf3486fcec98b0d5f4f311eaa32af5f3b976869
Roland Tritsch <roland@tritsch.email>
2022-01-15 18:25:41 +0000

    Day24: Part2: Done.

--- src/Day24.hs
+++ src/Day24.hs
@@ -14,17 +14,27 @@
 -- Part 1 - Look for all destination tiles that are there/where flipped
 -- an odd number of times.
 --
--- Part 2 - ???
+-- Part 2 - Take the result from part1 and start applying the rules
+-- day after day. To implement the rules we need to understand how many
+-- black tiles are adjacent to a given (black or white) tile (e.g. (count,
+-- position)). We can then apply the rules and flip the tiles accordingly.
+--
+-- Note: The grid has no boundaries. Means the number of white tiles is
+-- indefinte. But the number/list of black tiles is not. Means we will
+-- model the grid by means of a list of black tiles (all other tiles
+-- are (by definition) white).
 module Day24 where
 
-import Data.List (group, sort)
+import Data.List (group, sort, nub)
 import Data.Text (pack, unpack)
 import Text.Regex.Pcre2 (matchAll)
 import Util (inputRaw)
 import Prelude
 
 type Step = String
+
 type Steps = [Step]
+
 type Tiles = [Steps]
 
 data Position = Position Int Int
@@ -41,25 +51,85 @@ input filename = map processTile $ lines $ inputRaw filename
 walk :: Steps -> Position -> Position
 walk [] position = position
 walk ("e" : steps) (Position x y) = walk steps (Position (x + 1) y)
-walk ("se" : steps) (Position x y) = walk steps (Position (x + 1) (y -1))
-walk ("sw" : steps) (Position x y) = walk steps (Position x (y -1))
-walk ("w" : steps) (Position x y) = walk steps (Position (x -1) y)
-walk ("nw" : steps) (Position x y) = walk steps (Position (x -1) (y + 1))
+walk ("se" : steps) (Position x y) = walk steps (Position (x + 1) (y - 1))
+walk ("sw" : steps) (Position x y) = walk steps (Position x (y - 1))
+walk ("w" : steps) (Position x y) = walk steps (Position (x - 1) y)
+walk ("nw" : steps) (Position x y) = walk steps (Position (x - 1) (y + 1))
 walk ("ne" : steps) (Position x y) = walk steps (Position x (y + 1))
 walk _ _ = error "Invalid step"
 
 -- | Take a list of tile paths, flip them over and return a/the list of
 -- black tiles/positions.
-blackTiles :: Tiles -> [Position]
-blackTiles tiles = concat $ map snd $ filter (\(n, _) -> odd n) groupedByCount
+initialBlackTiles :: Tiles -> [Position]
+initialBlackTiles tiles = concat $ map snd $ filter (\(n, _) -> odd n) groupedByCount
   where
     destinationTiles = map (\steps -> walk steps (Position 0 0)) tiles
     groupedByCount = map (\positions -> (length positions, positions)) $ group $ sort destinationTiles
 
 -- | Solve part1.
 part1 :: Tiles -> Int
-part1 tiles = length $ blackTiles tiles 
+part1 tiles = length $ initialBlackTiles tiles
+
+-- | For a given tile, return the number of adjacent black tiles
+numberOfAdjacentBlackTiles :: Position -> [Position] -> Int
+numberOfAdjacentBlackTiles tile blackTiles = (length . filter (isAdjacent tile)) blackTiles
+
+-- | For a list of tiles, return the number of adjacent black tiles
+numberOfAdjacentBlackTiles' :: [Position] -> [Position] -> [(Int, Position)]
+numberOfAdjacentBlackTiles' tiles blackTiles = map process tiles
+  where
+    process tile = (numberOfAdjacentBlackTiles tile blackTiles, tile)
+
+-- | Check the rule and return the qualified tiles.
+checkRule :: ((Int, Position) -> Bool) -> [(Int, Position)] -> [Position]
+checkRule rule tiles = map snd $ filter rule tiles
+
+-- | Any black tile with zero or more than 2 black tiles immediately
+-- adjacent to it needs to be flipped (to white).
+blackRule :: (Int, Position) -> Bool
+blackRule (n, _)
+  | n == 0 || n > 2 = True
+  | otherwise = False
+
+-- | Any white tile with exactly 2 black tiles immediately
+-- adjacent to it needs to be flipped (to black).
+whiteRule :: (Int, Position) -> Bool
+whiteRule (n, _)
+  | n == 2 = True
+  | otherwise = False
+
+-- | Return all adjacent tiles for the given tile.
+allAdjacent :: Position -> [Position]
+allAdjacent (Position x y) =
+  [ Position (x + 1) y,
+    Position (x + 1) (y - 1),
+    Position x (y - 1),
+    Position (x - 1) y,
+    Position (x - 1) (y + 1),
+    Position x (y + 1)
+  ]
+
+-- | Tests, if a two tiles are adjacent.
+isAdjacent :: Position -> Position -> Bool
+isAdjacent thisTile thatTile = elem thisTile (allAdjacent thatTile)
+
+-- | For the given list of black tiles, return a/the list of white
+-- that need to be checked/considered.
+allWhiteTiles :: [Position] -> [Position]
+allWhiteTiles tiles = nub $ sort $ filter (not . flip elem tiles) $ foldl process [] tiles
+  where
+    process whites tile = whites ++ allAdjacent tile
+
+-- | Do a day of work.
+doDay :: [Position] -> [Position]
+doDay tiles = whiteTilesToBeFlippedToBlack ++ filter (not . flip elem blackTilesToBeFlippedToWhite) tiles
+  where
+    blackTilesToBeFlippedToWhite = checkRule blackRule (numberOfAdjacentBlackTiles' tiles tiles)
+    whiteTilesToBeFlippedToBlack = checkRule whiteRule (numberOfAdjacentBlackTiles' (allWhiteTiles tiles) tiles)
 
 -- | Solve part2.
 part2 :: Tiles -> Int
-part2 = length
+part2 tiles = length $ go (100 :: Int) (initialBlackTiles tiles)
+ where
+   go 0 tiles' = tiles'
+   go n tiles' = go (n - 1) (doDay tiles')
--- test/Day24Spec.hs
+++ test/Day24Spec.hs
@@ -14,10 +14,11 @@ run = hspec $ do
       let steps = ["se","se","nw","ne","ne","ne","w","se","e","sw","w","sw","sw","w","ne","ne","w","se","w","sw"]
       walk steps (Position 0 0) `shouldBe` Position (-1) (-2)
                  
-  describe "blackTiles" $ do
+  describe "initialBlackTiles" $ do
     it "testcases" $ do
       let expected = [Position (-2) 0,Position (-2) 1,Position (-1) (-2),Position (-1) (-1),Position (-1) 1,Position 0 (-3),Position 0 0,Position 0 3,Position 2 (-2),Position 2 0]
-      blackTiles (input "./input/Day24p1test.txt") `shouldBe` expected
+      
+      initialBlackTiles (input "./input/Day24p1test.txt") `shouldBe` expected
                  
   describe "part1" $ do
     it "testcases" $ do
@@ -25,10 +26,37 @@ run = hspec $ do
 
     it "puzzle" $ do
       part1 (input "./input/Day24p1.txt") `shouldBe` 269
+                 
+  describe "checkRule" $ do
+    it "testcases" $ do
+      let tiles = initialBlackTiles (input "./input/Day24p1test.txt")
+      let expected = [Position 0 3,Position 2 (-2),Position 2 0]
+      
+      checkRule blackRule (numberOfAdjacentBlackTiles' tiles tiles) `shouldBe` expected
+
+  describe "allWhiteTiles" $ do
+    it "simple" $ do
+      let tiles = [Position 0 0]
+      let expected = [Position (-1) 0,Position (-1) 1,Position 0 (-1),Position 0 1,Position 1 (-1),Position 1 0]
+      
+      allWhiteTiles tiles `shouldBe` expected
+
+    it "testcases" $ do
+      let tiles = initialBlackTiles (input "./input/Day24p1test.txt")
+      let expected = [Position (-3) 0,Position (-3) 1,Position (-3) 2,Position (-2) (-2),Position (-2) (-1),Position (-2) 2,Position (-1) (-3),Position (-1) 0,Position (-1) 2,Position (-1) 3,Position (-1) 4,Position 0 (-4),Position 0 (-2),Position 0 (-1),Position 0 1,Position 0 2,Position 0 4,Position 1 (-4),Position 1 (-3),Position 1 (-2),Position 1 (-1),Position 1 0,Position 1 1,Position 1 2,Position 1 3,Position 2 (-3),Position 2 (-1),Position 2 1,Position 3 (-3),Position 3 (-2),Position 3 (-1),Position 3 0]
+      
+      allWhiteTiles tiles `shouldBe` expected
+
+  describe "doDay" $ do
+    it "testcases" $ do
+      let tiles = initialBlackTiles (input "./input/Day24p1test.txt")
+      length tiles `shouldBe` 10
+      (length . doDay) tiles `shouldBe` 15 
+      (length . doDay . doDay) tiles `shouldBe` 12 
 
   describe "part2" $ do
     it "testcases" $ do
-      part2 (input "./input/Day24p1test.txt") `shouldBe` 20
+      part2 (input "./input/Day24p1test.txt") `shouldBe` 2208
 
     it "puzzle" $ do
-      part2 (input "./input/Day24p1.txt") `shouldBe` 315
+      part2 (input "./input/Day24p1.txt") `shouldBe` 3667
