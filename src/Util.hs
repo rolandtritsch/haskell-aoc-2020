1445ceba480261bcbb9aa9ec81a22bd08bb00ba9
Roland Tritsch <roland@tritsch.email>
2022-01-17 07:48:28 +0000

    Added a/the trace` function.

--- src/Util.hs
+++ src/Util.hs
@@ -2,8 +2,16 @@
 -- Various utility functions.
 module Util where
 
+import Debug.Trace
 import System.IO.Unsafe (unsafePerformIO)
 
--- | get the (raw) input (as one line (with newlines in it))
+-- | Get the (raw) input (as one line (with newlines in it)).
 inputRaw :: String -> String
 inputRaw fileName = unsafePerformIO $ readFile fileName
+
+-- | Nice(er) trace function.
+trace' :: Show a => String -> a -> a
+-- trace' _prefix what = what
+trace' prefix what = trace text what
+  where
+    text = prefix ++ (show what)
